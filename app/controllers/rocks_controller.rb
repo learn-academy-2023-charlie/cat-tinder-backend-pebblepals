02bcb33d5f5c7e029f91569002235d3ee5877a03
jricks86 <jricks86@gmail.com>
2023-06-06 11:03:40 -0700

    backend api complete

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -1,2 +1,22 @@
 class RocksController < ApplicationController
+    def index
+        rocks = Rock.all
+        render json: rocks
+    end
+
+    def create
+        rock = Rock.create(rock_params)
+        render json: rock
+    end
+
+    private
+    def rock_params
+        params.require(:rock).permit(:name, :age, :enjoys, :image)
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
--- spec/requests/rocks_spec.rb
+++ spec/requests/rocks_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "Rocks", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Rock.create(
+        name: 'Rocky Balboa',
+        age: "10,000",
+        enjoys: 'boxing and training sequences',
+        image: 'placeholder'
+      )
+      get '/rocks'
+
+      rock = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(rock.length).to eq 1
+    end
+  end
+  describe "POST /create" do
+    it "creates a rock" do
+      rock_params = {
+        rock: {
+        name: 'Rocky Balboa',
+        age: "10,000",
+        enjoys: 'boxing and training sequences',
+        image: 'placeholder'
+    }
+    }
+
+      post '/rocks', params: rock_params
+
+      expect(response).to have_http_status(200)
+      expect(rock.name).to eq 'Rocky Balboa'
+    end
   end
 end
